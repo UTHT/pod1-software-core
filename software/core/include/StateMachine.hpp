11969bcffe8342135728e134574e2e5c077f2555
Achu Mukundan <achuthanmukundan00@gmail.com>
2019-10-27 14:24:41 -0400

    Add StateMachine class

---
+++ software/core/include/StateMachine.hpp
@@ -0,0 +1,13 @@
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+
+class StateMachine {
+    private:
+    // member vars
+    public:
+    StateMachine();
+    ~StateMachine();
+    void run();
+};
+
+#endif
