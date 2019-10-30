b5a67cce0915ce96a7f24fb62c6267fa34d35a8a
Daman Sharma <damansh@gmail.com>
2019-10-30 14:20:44 -0400

    Added channel_monitor.hpp file

---
+++ software/core/include/channel_monitor.hpp
@@ -0,0 +1,23 @@
+#ifndef CHANNEL_MONITOR
+#define CHANNEL_MONITOR
+#include <lcm/lcm-cpp.hpp>
+#include "channels/practice.hpp"
+#include <string>
+
+class ChannelMonitor{
+
+    std::string channel_name;
+    
+    public:
+        ~ChannelMonitor(){}
+
+        void check(){
+
+        }
+
+        void publish(){
+
+        }
+
+}
+#endif
