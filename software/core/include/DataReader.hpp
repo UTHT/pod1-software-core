84fff302437f4a2b605272c1b5ed22e396c39cea
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-11-11 14:18:22 -0500

    Resolve TODOs

--- software/core/include/DataReader.hpp
+++ software/core/include/DataReader.hpp
@@ -4,7 +4,7 @@
 #include <sys/time.h>
 #include <stdbool.h>
 #include "helper/IOPin.hpp"
-// #include "Channel.hpp" // To be implemented by Sabita and Shaziah
+#include "Channel.hpp" // To be implemented by Sabita and Shaziah
 
 class DataReader{
   private:
@@ -13,19 +13,9 @@ class DataReader{
   public:
     DataReader();
     ~DataReader();
-    /*TODO: Decide whether using default params or diff paramter
-      defs is the way to go. IE, either use def as defined below
-      in the uncommented code or as follows:
-        bool get() -> non-blocking
-        bool get(const struct timeval timeout) -> blocking, with timeout*/
-
-    // a negative timeout waits forever
-    // TODO, technically, the blocking bool isn't necessary either
     //  a timeout = 0 means non-blocking, a timeout > 0 means block for whatever
     //  time, and a timeout < 0 means block forever
-    bool get(const bool blocking = false,
-             const struct timeval timeout ={0});
-    bool pub(const bool blocking = false,
-             const struct timeval timeout ={0});
+    bool get(const struct timeval timeout ={0});
+    bool pub(const struct timeval timeout ={0});
 };
 #endif
