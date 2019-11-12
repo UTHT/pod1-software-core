630a8658ce05daf76c8477b034c60cce82976e13
Rafay Kalim <rafayk7@yahoo.com>
2019-11-11 20:22:13 -0500

    fix directory structure and minor changes

--- software/core/include/helper/enums.hpp
+++ software/core/include/helper/enums.hpp
@@ -1,9 +1,13 @@
 #ifndef ARDUINOBOARDS_H
 #define ARDUINOBOARDS_H
 
+// TODO : Check if we should have separate enum files for different things
+
+// Channel Class
 enum channelStatus {noValue, outOfRange, lowFrequency, highFrequency, functioning};
 enum channelVariable {sensorValue, frequencyValue};
 
+// Arduino Enums
 enum ArduinoBoards {FRONT_ANALOG, REAR_ANALOG, MOTOR_CONTROLLER, BRAKE_CONTROLLER};
 
 #endif
