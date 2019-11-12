380ea13abde4b160c514b3f56742fe16fe3bbfed
Rafay Kalim <rafayk7@yahoo.com>
2019-11-11 19:40:08 -0500

    fixed directory structure and .hpp file

--- software/core/src/HealthMonitor.cpp
+++ software/core/src/HealthMonitor.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "HealthMonitor.hpp"
 
+// Just some testing code for now. Will describe full code for class here in later sprints
+
 int main(int argc, char const *argv[])
 {
    HealthMonitor healthMonitor;
