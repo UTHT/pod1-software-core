b9a4e05906808eab145ab5162b3408e437af3605
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-11-13 00:17:56 -0500

    Add Makefile and main.cpp

---
+++ software/core/src/main.cpp
@@ -0,0 +1,12 @@
+#include "Controller.hpp"
+#include "DataProcessor.hpp"
+#include "State.hpp"
+#include "StateMachine.hpp"
+#include "helper/enums.hpp"
+
+int main(int argc, char* argv[]){
+  Controller conroller();
+  DataProcessor data_processor();
+  State state();
+  StateMachine state_machine();
+}
