ef0c6b0c5d5336edeef630d5e3e028ed96a41e5e
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-11-13 00:02:10 -0500

    Fix syntax, styling, and imports : ChannelMonitor

--- software/core/include/ChannelMonitor.hpp
+++ software/core/include/ChannelMonitor.hpp
@@ -1,41 +1,37 @@
-#pragma once
+#ifndef CHANNELMONITOR_H
+#define CHANNELMONITOR_H
 
-using namespace std;
 
 #include <string>
 #include <vector>
-#include "/helper/enums.hpp"
-//#include "Channel"
 
-// Temporary class (for now)
-// testing purposes
-// class Channel {
-//     public:
-//         double sensorValue;
-//         double frequencyValue;
-// };
+#include "helper/enums.hpp"
+#include "Channel.hpp"
 
 class ChannelMonitor{
-    private:    
-        string channel_name;
+    private:
+        // string channel_name;
 
         // Extra private members
-        Channel channel_data;
-        double min_accepted_value;
-        double max_accepted_value;
-        double min_accepted_frequency;
-        double max_accepted_frequency;
-    
+        Channel channel;
+        double min_value;
+        double max_value;
+        double min_rtt;
+        double max_rtt;
+
     public:
+        // For testing...
+        ChannelMonitor();
         // Constructor to initialize the ChannelMonitor class
-        ChannelMonitor(double minAcceptedValue, double maxAcceptedValue, double minAcceptedFrequency, double maxAcceptedFrequency);
+        ChannelMonitor(Channel channel,
+                       double min_value, double max_value,
+                       double min_rtt, double max_rtt);
+        ~ChannelMonitor();
 
-        // TODO : Check if this should be public?
-        bool is_sensor_value_in_range(double channel_sensor_value);
-
-        // Check the channel value. Vector if there are multiple problems
-        vector<channelStatus> check();
+        int check();
 
         // Will edit this based on the Channel class implementation
-        void publish();
-}
+        bool publish();
+};
+
+#endif
