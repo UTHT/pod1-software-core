b9008fd72a0398585d8ed7ed9960899a03fc0073
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-11-13 00:02:41 -0500

    Fix syntax, styling, and imports : HealthMonitor

--- software/core/include/HealthMonitor.hpp
+++ software/core/include/HealthMonitor.hpp
@@ -3,28 +3,26 @@
 
 #include <string>
 #include <vector>
+
+#include "ChannelMonitor.hpp"
 // We will also need to include some JSON or YAML Reader Library to read the files for channel config data
 
-// Verifies system health and adequate data flow
-class HealthMonitor
-{
-private:
-   // Need a vector of ChannelMonitors to iterate through to check if channel is healthy
-   // TODO : Change with real class ChannelMonitor class name in Sprint 2
-   std::vector<TestChannelMonitor> m_readers; // maybe pointers to readers instead, however this comes with a performance cost
+class HealthMonitor{
+  private:
+   std::vector<ChannelMonitor> channel_monitors; // maybe pointers to readers instead, however this comes with a performance cost
 
+   // TODO (Mathieu) Why is the below necessary? Confused.
    // Need ENUM variable for current state. For now, it is commented out as we will only consider E-stop until details are hashed out
    // ENUM_STATE m_state;
-   bool m_estop;
+   // bool m_estop;
 
-public:
-   /*
-      num_channel_monitors : how many channels are to be tested
-      path : path to config file 
-   */
-   HealthMonitor(size_t num_channel_monitors = 0, const std::string &path = "");
+  public:
+   // For testing
+   HealthMonitor();
+   HealthMonitor(const std::string &file_name);
    ~HealthMonitor();
 
    // Describes High Level Functionality
    void run();
 };
+#endif
