868226783e4ddb263988bd68fb423b8aad220d73
ayushishri <32212671+ayushishri@users.noreply.github.com>
2019-11-25 23:02:56 -0500

    Functioning dial

--- dial/dial.py
+++ dial/dial.py
@@ -1,38 +1,3 @@
-# import sys
-# import PyQt5
-# from PyQt5 import QtGui,QtCore, QtWidgets
-# from PyQt5.QtGui import QIcon
-# from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QDial, QHBoxLayout, QSpinBox
-#
-# class Window(QDialog):
-#     def __init__(self):
-#         super().__init__(dialvalue,Max,Min)
-#
-#         self.setWindowTitle("Temperature Dial")
-#         self.setGeometry(40,400,450,300)
-#         self.setWindowIcon(QIcon("dial.py"))
-#         self.InitUI(Max,Min)
-#
-#
-#     def InitUI(self,Max,Min):
-#         dial = QDial()
-#         dial.setNotchesVisible(True)
-#         dial.setMinimum(Min)
-#         dial.setMaximum(Max)
-#         spin = QSpinBox()
-#         hbox = QHBoxLayout()
-#         hbox.addWidget(dial)
-#         hbox.addWidget(spin)
-#         self.setLayout(hbox)
-#         dial.valueChanged.connect(spin.setValue)
-#         spin.valueChanged.connect(dial.setValue)
-#
-# app = QApplication(sys.argv)
-# window = Window()
-# window.show()
-# app.exec()
-
-
 from PyQt5.QtWidgets import *
 import sys
 
@@ -40,22 +5,22 @@ class Window(QWidget):
     def __init__(self,Max,Min,Val):
         QWidget.__init__(self)
         layout = QGridLayout()
+        self.spin = QSpinBox()
         self.setLayout(layout)
         self.dial = QDial()
         self.dial.setNotchesVisible(True)
         self.dial.setMinimum(Min)
         self.dial.setMaximum(Max)
         self.dial.setValue(Val)
+        self.spin.setValue(Val)
         layout.addWidget(self.dial)
+        layout.addWidget(self.spin)
+        self.dial.valueChanged.connect(self.spin.setValue)
+        self.spin.valueChanged.connect(self.dial.setValue)
 
-    def updateValue(self,new):
-        self.dial.valueChanged.connect(new)
 
 app = QApplication(sys.argv)
 screen = Window(100,0,20)
 screen.show()
-# for i in range(5):
-#     new = input("New Value? = ")
-#     screen.updateValue(int(new))
 sys.exit(app.exec_())
 
