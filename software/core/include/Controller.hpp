a24b7a4207b39bd794c6615c47608dce0ac70beb
urmilmodi <urmilnmodi@gmail.com>
2019-12-25 00:38:15 -0500

    Made the base Controller class abstract

--- software/core/include/Controller.hpp
+++ software/core/include/Controller.hpp
@@ -1,38 +1,34 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <Arduino.h>
 #include <stdio.h>
 #include <vector>
 
+#include "State.hpp"
+#include "Controller.hpp"
 #include "helper/enums.hpp"
 #include "helper/IOPin.hpp"
-#include "Channel.hpp"
-#include "State.hpp"
 // #include "../lib/Arduino-PID-Library/PID_v1.h"
-
-// TODO: Need to add functionality if sensors measurements occur with the same Arduino
-// Sensor class?
 
 class Controller {
   private:
     // defined in enums
     struct IOPin io_pin;
     States current_state;
-    int total_states;
     std::vector<Channel> channels;
 
   public:
     
     Controller(const struct IOPin io_pin,
                const States current_state,
-               const int total_states,
                const std::vector<Channel> channels);
 
     virtual ~Controller();
     int state_change(const States change_to);
     // Run through list of channels for updated sensor values and command instructions
-    virtual int run();
+    virtual int run() = 0;
    // ESTOP command
-    virtual bool shutdown();
+    virtual bool shutdown() = 0;
 };
 #endif
--- software/core/src/Controller.cpp
+++ software/core/src/Controller.cpp
@@ -1,9 +1,4 @@
-#if ARDUINO >= 100
-  #include "Arduino.h"
-#else
-  //#include "WProgram.h"
-#endif
-
+#include <Arduino.h>
 #include <stdio.h>
 #include <vector>
 
@@ -15,29 +10,18 @@
 Controller::Controller(
     const struct IOPin io_pin,
     const States current_state,
-    const int total_states,
-    const std::vector<Channel> channels)    
+    const std::vector<Channel> channels)
 {
 
-    if (total_states > 0) {
-        
-      this->current_state = current_state;
-      this->total_states = total_states;
-    }
-    // TODO
-    //else {ERROR HANDLING}
-
+    this->current_state = current_state;
     this->io_pin = io_pin;
     this->channels = channels;
 
     // Communications Channel Reader Object Declarations, etc
 }
 
-Controller::~Controller() = default;
-
-int Controller::state_change(const States change_to){
+int Controller::state_change(const States change_to) {
   
-
   this->current_state = change_to;
   return 0;
 }
