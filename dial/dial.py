a8014bd9a995ebaddf186946f1856e84f0e60fd8
ayushishri <32212671+ayushishri@users.noreply.github.com>
2020-01-18 15:38:11 -0500

    Dial with update function

--- dial/dial.py
+++ dial/dial.py
@@ -1,8 +1,8 @@
 from PyQt5.QtWidgets import *
 import sys
 
-class Window(QWidget):
-    def __init__(self,Max,Min,signallist):
+class dial(QWidget):
+    def __init__(self,Max,Min,Val):
         QWidget.__init__(self)
         layout = QGridLayout()
         self.spin = QSpinBox()
@@ -11,16 +11,22 @@ class Window(QWidget):
         self.dial.setNotchesVisible(True)
         self.dial.setMinimum(Min)
         self.dial.setMaximum(Max)
-        self.dial.setValue(signallist[len(signallist)-1])
-        self.spin.setValue(signallist[len(signallist)-1])
+        self.dial.setValue(Val)
+        self.spin.setValue(Val)
         layout.addWidget(self.dial)
         layout.addWidget(self.spin)
         self.dial.valueChanged.connect(self.spin.setValue)
         self.spin.valueChanged.connect(self.dial.setValue)
 
+    def update(self,newval):
+        self.dial.valueChanged(newval)
+
+
+
 
 app = QApplication(sys.argv)
-screen = Window(100,0,[20,30,40])
-screen.show()
+d = dial(100,0,50)
+d.show()
+# d.update(50)
 sys.exit(app.exec_())
 
