9f37004f1cdfdc905fe47c2cd92ab5dd3b96d926
Joseph Sawaya <josephsawaya938@gmail.com>
2020-01-18 00:07:53 -0500

    First iteration of table class
    The table successfully displays. Width, height, title, number of
    rows and columns are all customizable. It is not dynamic yet.

--- software/core/python/src/control-panel/Table.py
+++ software/core/python/src/control-panel/Table.py
@@ -1,39 +1,72 @@
-'''
-Control Panel Table Widget
-'''
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from typing import Dict
 
-
-class Table():
-    def __init__(self) -> None:
-        ...
-
-    def __str__(self) -> str:
-        ...
+class Table(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel()
+        self.table = QTableWidget()
+        self.columns = {}
+        self.rows = {}
 
     def set_title(self, title: str) -> bool:
-        ...
-
+        self.label.setText(title)
+    
     def set_columns(self, num_columns: int) -> bool:
-        ...
-
+        if(num_columns < 1):
+            return False
+        else: 
+            self.table.setColumnCount(num_columns)
+            return True
+    
     def set_rows(self, num_rows: int) -> bool:
-        ...
+        if(num_rows < 1):
+            return False
+        else:
+            self.table.setRowCount(num_rows)
+            return True
 
-    # NOTE : these two should have checking to make sure it fits in width for
-    #        example
     def set_column_width(self, column_number: int, width: float) -> bool:
-        ...
+        if(column_number >= self.table.columnCount()):
+            return False
+        elif((sum(self.columns.values()) + width) > self.width):
+            return False
+        else:
+            self.columns[column_number] = width
+            return True
 
-    def set_row_height(self, column_number: int, width: float) -> bool:
-        ...
+    def set_row_width(self,row_number: int, height: float) -> bool:
+        if(row_number >= self.table.rowCount()):
+            return False
+        elif((sum(self.rows.values()) + height) > self.height):
+            return False
+        else:
+            self.rows[row_number] = height
+            return True
 
     def set_width(self, width: float) -> bool:
-        ...
+        self.width = width
 
     def set_height(self, height: float) -> bool:
-        ...
+        self.height = height
 
-    # TODO : This should actually return the actual PyQT (or whatever lib we
-    #       use) widget, not None. Not sure if this is the best way
-    def display(self) -> None:
-        ...
+    def display(self) -> QWidget:
+        for x, y in self.columns:
+            self.table.setColumnWidth(x,y)
+        for x, y in self.rows:
+            self.table.setRowHeight(x,y)
+        for i in range(self.table.rowCount()):
+            for j in range(self.table.columnCount()):
+                self.table.setItem(i,j, QTableWidgetItem("Cell (%s,%s)" % (i,j)))
+                self.table.item(i,j).setFlags(Qt.NoItemFlags)
+        self.table.horizontalHeader().hide()
+        self.setGeometry(0, 0, self.width, self.height)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.table) 
+        self.setLayout(self.layout) 
+        self.show()
+        
+    
