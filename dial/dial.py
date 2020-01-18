bc246506f18a6b4c31adbbb826b1b2964e751700
ayushishri <32212671+ayushishri@users.noreply.github.com>
2020-01-18 15:02:28 -0500

    Dynamic Dial
    takes a list of values and displays the last value

--- dial/dial.py
+++ dial/dial.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import *
 import sys
 
 class Window(QWidget):
-    def __init__(self,Max,Min,Val):
+    def __init__(self,Max,Min,signallist):
         QWidget.__init__(self)
         layout = QGridLayout()
         self.spin = QSpinBox()
@@ -11,8 +11,8 @@ class Window(QWidget):
         self.dial.setNotchesVisible(True)
         self.dial.setMinimum(Min)
         self.dial.setMaximum(Max)
-        self.dial.setValue(Val)
-        self.spin.setValue(Val)
+        self.dial.setValue(signallist[len(signallist)-1])
+        self.spin.setValue(signallist[len(signallist)-1])
         layout.addWidget(self.dial)
         layout.addWidget(self.spin)
         self.dial.valueChanged.connect(self.spin.setValue)
@@ -20,7 +20,7 @@ class Window(QWidget):
 
 
 app = QApplication(sys.argv)
-screen = Window(100,0,20)
+screen = Window(100,0,[20,30,40])
 screen.show()
 sys.exit(app.exec_())
 
