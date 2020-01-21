6e292600688661643d688f663fbea7223518a1f6
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-01-21 13:45:23 -0500

    Working on StateMachine constructor

--- software/core/include/StateMachine.hpp
+++ software/core/include/StateMachine.hpp
@@ -1,12 +1,27 @@
 #ifndef STATEMACHINE_H
 #define STATEMACHINE_H
 #include "helper/enums.hpp"
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 class StateMachine {
   private:
     States current_state;
+    double max_distance;
+    double max_motor_temperature;
+    double max_front_wheel_temperature;
+    double max_battery_temperature;
+    double min_battery_level;
+    double min_gap_height_front;
+    double min_gap_height_rear;
+    double max_vibration;
+    double min_air_pressure_reserve;
+    double min_air_pressure_secondary;
+
   public:
-  StateMachine();
+  StateMachine(const fs::path &config_path);
   ~StateMachine();
   void run();
 };
--- software/core/src/StateMachine.cpp
+++ software/core/src/StateMachine.cpp
@@ -1,9 +1,25 @@
+#include <fstream>
+#include <stdexcept>
 #include "StateMachine.hpp"
 
-StateMachine::StateMachine(){
+StateMachine::StateMachine(const fs::path &config_path){
+  if (!fs::exists(config_path))
+    throw std::invalid_argument("StateMachine: The file config path does not exist.");
   current_state = OFF;
+  max_distance = 0;
+  max_motor_temperature = 0;
+  max_front_wheel_temperature = 0;
+  max_battery_temperature = 0;
+  min_battery_level = 0;
+  min_gap_height_front = 0;
+  min_gap_height_rear = 0;
+  max_vibration = 0;
+  min_air_pressure_reserve = 0;
+  min_air_pressure_secondary = 0;
 }
 StateMachine::~StateMachine(){
 }
 void StateMachine::run(){
+  while (true){
+  }
 }
