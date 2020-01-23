b9230c6c188b09a18e08886aa3b35d23bfaef8c9
Erebus <erebus_illyrium@github.com>
2020-01-23 18:19:39 -0500

    Modified EstopState.cpp to include the lcm_channel_handler

--- software/core/src/EstopState.cpp
+++ software/core/src/EstopState.cpp
@@ -8,46 +8,19 @@ EstopState::EstopState():State(){}
 //primary constructor
 EstopState::EstopState(States name):State(name){
     this->name = name;
-    this->launch_channel_ptr = new Channel("data-launch-ready");
-    this->check_emergency_ptr = new Channel("command-estop");
+    
+    estop_info.raised_state_name = " "
 }
 
 
 //function to request data from States 
 States EstopState::run(){
-
-    //TODO: We currently assume that the 'get()' function
-    //      returns a bool. We will most likely need to update
-    //      this syntax to reflect changes to the channel class
-    //      in the future. (e.g. template parameters)
-
-    //first check for emergency break triggers
-    if (check_emergency_ptr->get()){
-        return ESTOP;
-    }
-
-    //check for run trigger
-    if (launch_channel_ptr->get()){
-        return DRIVE;
-    }
-    
-    //default case is to stay in Arm State
-    return ARM;
+    //pass struct private member of class to LCM handler
+    lcm_channel_handler(&(this->estop_info));
 }
 
 
 //destructor
 EstopState::~EstopState(){
 
-    //delete channel objects if they exist
-
-    if (this->launch_channel_ptr != NULL){
-        delete this->launch_channel_ptr;
-        this->launch_channel_ptr = NULL;
-    }
-
-    if (this->check_emergency_ptr != NULL){
-        delete this->check_emergency_ptr;
-        this->check_emergency_ptr = NULL;
-    }
 }
