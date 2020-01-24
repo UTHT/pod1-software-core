fa2ea1a0f9d211d05fed00003ea8389cbf506e40
Joseph Sawaya <josephsawaya938@gmail.com>
2020-01-23 22:37:45 -0500

    Creation of digestor function Definition of digestor function in Digestor.py. Used to parse through json data received from sensors. Receives list of widgets as input, updates widgets with data from json file. Two new classes in components.py TableLocation and TableEnum used in Digestor.py for readability. Data.json is example data for Digestor.py

--- software/core/python/src/control-panel/components.py
+++ software/core/python/src/control-panel/components.py
@@ -1,7 +1,17 @@
 from typing import NamedTuple
-
+from enum import Enum
 
 class Colour(NamedTuple):
     red: float
     green: float
     blue: float
+
+
+class TableLocation(NamedTuple):
+    row: int
+    col: int
+
+class TableEnum(Enum):
+    TABLE: 0
+    ROW: 1
+    COL: 1
