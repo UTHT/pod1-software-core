722fbfd925480c2cfa9f069710efef25830df7e7
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-01-25 16:23:29 -0500

    Changes from Layout

--- software/core/python/src/control-panel/components.py
+++ software/core/python/src/control-panel/components.py
@@ -1,4 +1,5 @@
 from typing import NamedTuple
+from enum import Enum
 
 
 class Colour(NamedTuple):
@@ -10,3 +11,11 @@ class Colour(NamedTuple):
 class Coordinate(NamedTuple):
     x: int
     y: int
+
+
+class LayoutOrientation(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 0
+
+    def __str__(self) -> str:
+        return self.name
