785308d114aae3e90c9a1fbb98c5bd7054962d07
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-01-27 13:58:13 -0500

    Fix issues with SystemHealthState
    - some subclassing issues and declaration of methods

--- software/core/include/SystemHealthState.hpp
+++ software/core/include/SystemHealthState.hpp
@@ -9,21 +9,17 @@
 #include "NetworkMonitor.hpp"
 #include "DataReader.hpp"
 #include "State.hpp"
+#include "helper/enums.hpp"
 //add states enum.hpp
 
-class SystemHealthState:public State{
-		private:
-	        States name,
-		//The data pertaining to the system.
-		//Get the data from data reader and health monitor
-                std::vector<HealthMonitor> health_monitors;
-		
-		public:
-		SystemHealthState();
-		~SystemHealthState();
-		void sys_check();
-		//if sys_check=1, go to arm state
-		//elseif sys_check=0, go to debug state
-		void run(); 
+class SystemHealthState: public State{
+  private:
+    std::vector<HealthMonitor> health_monitor;
+
+	public:
+	  SystemHealthState(States name);
+	  ~SystemHealthState();
+    States run();
+
 };
 #endif
--- software/core/src/SystemHealthState.cpp
+++ software/core/src/SystemHealthState.cpp
@@ -1,26 +1,17 @@
 #include "SystemHealthState.hpp"
-	
 
-SystemHealthState::SystemHealthState(){
+SystemHealthState::SystemHealthState(States name) : State(name){
+
 }
-SystemHealthState::SystemHealthState(){
-		        //Define parametrs and required values to check
-}
-		
 
 SystemHealthState::~SystemHealthState(){
 }
-					
-States SystemHealthState::syscheck(){
-			void.get();//Get the list of parameters from health monitor. 
-			//Create a if loop for checking the values.
-			//notify the problematic parameter.
-     			//create a counter k++
-}
+
 States SystemHealthState::run(){
-	 						          				//if k=n,Go to SystemHealthCheck
-		//state = Arm
-    		//else
- 		//state = Debug
-									          											}
-	
+  //if k=n,Go to SystemHealthCheck
+  //state = Arm
+  //else
+  //state = Debug
+  return SYS_CHECK;
+}
+
