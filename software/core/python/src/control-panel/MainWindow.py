9a98a1570e7350eecdc55ecf34f9f08a9a6811d5
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-01-27 11:50:21 -0500

    Minor refactoring to Window class

--- software/core/python/src/control-panel/MainWindow.py
+++ software/core/python/src/control-panel/MainWindow.py
@@ -1,17 +1,22 @@
+from typing import Any
 from PyQt5.QtWidgets import QWidget, QTabWidget, QMainWindow
 from Layout import Layout
 
 
-class MainWindow(QMainWindow):
-    def __init__(self, left: int, top: int,
-                 width: int, height: int) -> None:
-        super().__init__()
+class Window(QMainWindow):
+    def __init__(self,
+                 title: str,
+                 left: int,
+                 top: int,
+                 width: int,
+                 height: int,
+                 parent: Any = None) -> None:
+        super(Window, QMainWindow).__init__(parent)
         self.tabs = QTabWidget()
         self.setCentralWidget(self.tabs)
-        self.title = 'Control Panel'
+        self.title = title
         self.setWindowTitle(self.title)
         self.setGeometry(left, top, width, height)
-        self.setWindowTitle(self.title)
         self.show()
 
     def new_tab(self, layout: Layout, tab_name: str) -> None:
