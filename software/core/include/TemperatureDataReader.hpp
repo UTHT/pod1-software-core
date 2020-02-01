bb904a36995736e110180cda141fc8bea02a6c02
sabitatasnim <44930670+sabitatasnim@users.noreply.github.com>
2020-02-01 14:14:08 -0500

    Definition of TemperatureDataReader class-.hpp file is written

---
+++ software/core/include/TemperatureDataReader.hpp
@@ -0,0 +1,24 @@
+#ifndef TEMPERATURE_READER_HPP
+#define TEMPERATURE_READER_HPP
+
+#include "DataReader.hpp"
+
+class TemperatureDataReader: public DataReader{
+    private:
+        //the temperature
+        double temperature; 
+        //both ends of the voltages 
+        double max_volt; 
+        double min_volt; 
+        int analog_input; 
+
+    public:
+    //primary constructor
+    TemperatureDataReader(DataReader IOPin);
+    //destructor
+    ~TemperatureDataReader();
+    //reads the temperature and updates object
+    void TemperatureDataReader::readTemperature(); 
+};
+
+#endif
