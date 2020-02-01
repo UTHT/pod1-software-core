d095caad42bd5809c52a3d55c5ece9c8239fc042
SEERANGA_NANDHINI SAYEE <nandhini18@NandhiniJarvis.localdomain>
2020-02-01 14:21:28 -0500

    Fix the functions

--- software/core/src/CommunicationCheckState.cpp
+++ software/core/src/CommunicationCheckState.cpp
@@ -7,9 +7,20 @@ CommunicationCheckState::~CommunicationCheckState(){
 }
 
 States CommunicationCheckState::run(){
-	//if k=n,Go to SystemHealthCheck
-	//state = SysCheck
-	//else
-	//state = CommsCheck
+	//for(int i = 0;i < channels.size() ; i++)
+	//{
+	//	if(channels.at(i) exists) TODO:Find some function that compares the 	            received signal with a pre-defined list 
+	//	{
+	//	count the received signals(use j++)
+	//	return j;
+	//	}
+	//	else
+	//      {
+	//      count the missed channel(use k++)
+	//      cout << "Channel " << i << "is not responding."<<endl;
+	//      return k;
+	//	}
+	//}
+	//cout <<"Responding channels :"<< j <<"\n Missing channels:"<< k;
   return COMMS_CHECK;
 }
--- software/core/src/SystemHealthState.cpp
+++ software/core/src/SystemHealthState.cpp
@@ -1,5 +1,5 @@
 #include "SystemHealthState.hpp"
-
+using namespace std;
 SystemHealthState::SystemHealthState(States name) : State(name){
 
 }
@@ -8,10 +8,30 @@ SystemHealthState::~SystemHealthState(){
 }
 
 States SystemHealthState::run(){
-  //if k=n,Go to SystemHealthCheck
-  //state = Arm
+  //for(int i = 0;i< States name.size();i++)
+  //{
+  //	if(min_valuee<name[i]<max_value)
+  //	{
+  //	int j++ (count for healthy states)
+  //	return j;
+  //	}
+  //	else
+  //	{
+  //	cout<< "State"<<i<<"is unstable.";
+  //	k++(count for unstable states)
+  //	return k;
+  //	}
+  //cout<<"Healthy States:"<<j<<"\n Unhealthy States:"<<k<<endl; 
+  //}
+  //if (k=names.size) i.e all states are healthy
+  //{
+  // GOTO ARM STATE
+  // cout<<"System is healthy!!"<<endl;
+  //}
   //else
-  //state = Debug
+  //{
+  //GOTO DEBUG STATE;
+  //}
   return SYS_CHECK;
 }
 
