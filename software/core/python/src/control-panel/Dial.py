d38b8a8a00907e6d9b6bd5635dbb03dbc339eb3b
ayushishri <32212671+ayushishri@users.noreply.github.com>
2020-02-02 18:09:19 -0500

    Final dial with 5 total Notches

--- software/core/python/src/control-panel/Dial.py
+++ software/core/python/src/control-panel/Dial.py
@@ -4,7 +4,6 @@ Control Panel Dial Widget
 from typing import Any, Union
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5 import QtCore
 from PyQt5.QtGui import QPainter, QBrush, QPen, QFont
 import math
 from PyQt5.QtCore import Qt, QPoint
@@ -52,37 +51,50 @@ class Dial(QDial):
         self.current = current
         return True
 
+    def polartocart(self,degtorotate,needlelength):
+        newdegrees = 225 - degtorotate
+        x = needlelength * math.cos(newdegrees * math.pi / 180)
+        y = needlelength * math.sin(newdegrees * math.pi / 180)
+        return x,y
+
     def paintEvent(self, event):
         painter = QPainter(self)
         pixmap = QPixmap("dialbackground.png")
-
-
         painter.drawPixmap(self.rect(),pixmap)
 
         painter.setFont(QFont('Open Sans', self.diameter/40, weight=QFont.Bold))
-        painter.setPen(QPen(Qt.white, 8))
+        painter.setPen(QPen(Qt.white, self.diameter/50))
 
+        # Marking min, max and current values on the dial
         minpos = QPoint(self.diameter * 0.22, self.diameter * 0.85)
         painter.drawText(minpos, str(self.minval))
         maxpos = QPoint(self.diameter * 0.70, self.diameter * 0.85)
         painter.drawText(maxpos, str(self.maxval))
-        midpoint = QPoint(self.diameter / 2, self.diameter/10*6)
+        midpoint = QPoint(self.diameter/2 - self.diameter/40, self.diameter/10*6)
         painter.drawText(midpoint, str(self.current))
 
+        # Math for rotating needle to new poisition
         needlelength = math.sqrt((self.diameter/2-self.diameter*0.25)**2+(self.diameter/2-self.diameter*0.75)**2)
         diff = self.maxval-self.minval
         degpertick = 270/diff
         numtickstomove = self.current - self.minval
         degtorotate = degpertick * numtickstomove
 
-        newdegrees = 225 - degtorotate
-        x = needlelength * math.cos(newdegrees*math.pi/180)
-        y = needlelength * math.sin(newdegrees*math.pi/180)
-
+        x,y = self.polartocart(degtorotate , needlelength)
         painter.drawLine(self.diameter / 2, self.diameter / 2, (self.diameter/2)+x, (self.diameter/2)-y)
 
+        # Notches calculations
+        x,y = self.polartocart(56.25, needlelength*1.3)
+        tickpos1 = QPoint((self.diameter/2)+x, (self.diameter/2)-y)
+        painter.drawText(tickpos1, str(self.minval + (self.maxval - self.minval) / 4))
+        tickpos2 = QPoint(self.diameter / 2 - self.diameter / 40, self.diameter / 10 * 1.5)
+        painter.drawText(tickpos2, str(self.minval + (self.maxval - self.minval) / 2))
+        x, y = self.polartocart(213.75, needlelength)
+        tickpos3 = QPoint((self.diameter/2)+x, (self.diameter/2)-y)
+        painter.drawText(tickpos3, str(self.minval + (self.maxval - self.minval) / 4*3))
 
-app = QApplication(sys.argv)
-d = Dial (title = "dial", top = 50, left = 50, diameter = 400, min_value=10,max_value=50)
-d.set_current(40)
-sys.exit(app.exec_())
+
+# app = QApplication(sys.argv)
+# d = Dial (title = "dial", top = 50, left = 50, diameter = 300, min_value=10,max_value=50)
+# d.set_current(20)
+# sys.exit(app.exec_())
