8d7c9b047b09563f331d099a5447062134f57e65
MJavaadAkhtar <javaad.akhtar@mail.utoronto.ca>
2020-09-30 15:17:07 -0400

    Websocket BoilerPlate created

---
+++ software/core/Infrastructure/backend/index.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws');
+      
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
