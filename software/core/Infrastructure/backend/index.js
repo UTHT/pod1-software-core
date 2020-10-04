737e9e96b3a41ee6dfd8bf5c5178050bbf858532
Jimmy Lam <JimmyLam978@gmail.com>
2020-10-04 19:51:48 -0400

    added the heartbeat function in server

--- software/core/Infrastructure/backend/index.js
+++ software/core/Infrastructure/backend/index.js
@@ -1,13 +1,23 @@
-const WebSocket = require('ws');
-      
+const WebSocket = require("ws");
+const heartbeats = require("heartbeats");
+const heart = heartbeats.createHeart(1000);
+
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  });
+wss.on("connection", function connection(ws) {
+	ws.on("message", function incoming(data) {
+		wss.clients.forEach(function each(client) {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});
+
+	heart.createEvent(1, (count, last) => {
+		wss.clients.forEach(function each(client) {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(JSON.stringify({ message: "hello" }));
+			}
+		});
+	});
 });
