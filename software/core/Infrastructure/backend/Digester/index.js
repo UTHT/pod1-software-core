06fd090036ba9219289c8e2c7619f147258512c9
Jimmy Lam <JimmyLam978@gmail.com>
2020-10-31 16:08:15 -0400

    added digester function

---
+++ software/core/Infrastructure/backend/Digester/index.js
@@ -0,0 +1,29 @@
+const BatterySensor = require("../Classes/battery-sensor");
+const PositionSensor = require("../Classes/position-sensor");
+const SpeedSensor = require("../Classes/speed-sensor");
+const TempSensor = require("../Classes/temp-sensor");
+
+const fs = require("fs");
+
+function digest(odroid_json) {
+	const odroid_object = JSON.parse(odroid_json);
+	const { speed, temperatures, battery, position } = odroid_object;
+
+	const Speed = new SpeedSensor("speed", speed);
+	const Battery = new BatterySensor("battery", battery);
+	const Position = new PositionSensor("position", position[0], position[1]);
+	const temp_array = temperatures.map(
+		({ name, value }) => new TempSensor(name, value)
+	);
+
+	return [Speed, Battery, Position, ...temp_array];
+}
+
+const data = fs.readFileSync("test.json");
+
+const digester_object = digest(data);
+console.log(digester_object);
+
+module.exports = {
+	digest,
+};
