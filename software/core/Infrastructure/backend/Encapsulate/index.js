e225f950159dd93ddf5017458fbbd97111e34722
Jimmy Lam <JimmyLam978@gmail.com>
2020-11-07 13:37:49 -0500

    encapsulate function

---
+++ software/core/Infrastructure/backend/Encapsulate/index.js
@@ -0,0 +1,40 @@
+const BatterySensor = require("../Classes/battery-sensor");
+const PositionSensor = require("../Classes/position-sensor");
+const SpeedSensor = require("../Classes/speed-sensor");
+const TempSensor = require("../Classes/temp-sensor");
+
+const fs = require("fs");
+
+const { digest } = require("../Digester");
+const classes_constant = require("../constant/classes");
+
+function encapsulate(digester_obj) {
+	let obj = {};
+
+	digester_obj.map((sensor) => {
+		const type = sensor.sensor_type;
+		if (!obj.hasOwnProperty(type)) {
+			obj = { ...obj, [type]: [sensor] };
+		} else {
+			const current_arr = obj[type];
+			current_arr.push(sensor);
+			obj = { ...obj, [type]: current_arr };
+		}
+	});
+
+	return obj;
+}
+
+//testing
+const data = fs.readFileSync("test.json");
+
+console.log(data);
+const digester_object = digest(data);
+console.log(digester_object);
+
+const enc_obj = encapsulate(digester_object);
+console.log(enc_obj);
+
+module.exports = {
+	encapsulate,
+};
