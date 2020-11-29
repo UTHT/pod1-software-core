139a690db60fa7f8e35551d35a32655afc871857
Jimmy Lam <JimmyLam978@gmail.com>
2020-11-29 18:14:35 -0500

    reorganized podstate component

--- software/core/Infrastructure/AngularFrontend/src/app/components/podstate/podstate.component.ts
+++ software/core/Infrastructure/AngularFrontend/src/app/components/podstate/podstate.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./podstate.component.css']
 })
 export class PodstateComponent implements OnInit {
+  podstateData: any = {
+    startup: [
+      {name: 'Off', isActive: false, css_position: 'topright' },
+      {name: 'Debug Mode', isActive: false, css_position: 'midleft'},
+      {name: 'Comms Check', isActive: false, css_position: 'midright'},
+      {name: 'System Check', isActive: false, css_position: 'bottomright'}
+    ],
+    shutdown: [
+      {name: 'Shutdown', isActive: false, css_position: 'topleft'},
+    ],
+    ready: [
+      {name: 'Lock', isActive: false, css_position: 'topright'},
+      {name: 'Arms', isActive: false, css_position: 'bottomright'},
+    ],
+    active: [
+      {name: 'Braking', isActive: false, css_position: 'topleft'},
+      {name: 'Emergency Stop', isActive: false, css_position: 'midright'},
+      {name: 'Drive', isActive: false, css_position: 'bottomleft'},
+    ],
+  }
 
   constructor() { }
 
