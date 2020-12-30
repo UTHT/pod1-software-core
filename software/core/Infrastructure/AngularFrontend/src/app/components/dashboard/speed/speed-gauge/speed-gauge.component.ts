ee4f98de40ce949261d073f4031e2baeec18cbd8
BauerJustin <justin.bauer@mail.utoronto.ca>
2020-12-30 15:42:26 -0700

    Speed module

--- software/core/Infrastructure/AngularFrontend/src/app/components/dashboard/speed/speed-gauge/speed-gauge.component.ts
+++ software/core/Infrastructure/AngularFrontend/src/app/components/dashboard/speed/speed-gauge/speed-gauge.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
  
 @Component({
     selector: 'speed-gauge-component',
     templateUrl: './speed-gauge.component.html'
 })
 export class SpeedGaugeComponent {
-    
+    @Input()
+    speed: number;
+
     gaugeType = "arch";
-    gaugeValue = 0
+    gaugeValue = 0;
     gaugeLabel = "Speed";
     gaugeAppendText = "km/h";
 
-    // public randomize(): void {
-
-    //     this.gaugeValue += 0.1*(Math.round(Math.random()) * 4 - 1);
-    //     setTimeout(() => { this.randomize(); }, 50);
-    // }
+    ngOnInit() {
+        this.gaugeValue = this.speed;
+        this.getSpeed();
+    }
+    getSpeed() {
+        this.gaugeValue = this.speed | 0;
+        setTimeout(() => { this.getSpeed(); }, 50);
+    }
 }
--- software/core/Infrastructure/AngularFrontend/src/app/components/dashboard/speed/speed.component.ts
+++ software/core/Infrastructure/AngularFrontend/src/app/components/dashboard/speed/speed.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'speed-component',
   templateUrl: './speed.component.html',
-//   styleUrls: ['./speed.component.css']
+  //   styleUrls: ['./speed.component.css']
 })
 export class SpeedComponent implements OnInit {
+  currSpeed = 0;
+  speedArray = [[0,0]];
+  count = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.randomize();
   }
-
+  randomize() {
+    // Gauge updates every 50ms
+    // Graph updates every 5s
+    this.currSpeed += 0.1 * (Math.round(Math.random()) * 4 - 1);
+    this.count += 50;
+    if (this.count % 5000 == 0){
+      this.speedArray.push([this.count/1000,Math.floor(this.currSpeed)])
+      if (this.speedArray.length > 10){
+        this.speedArray.shift();
+      }
+    }
+    setTimeout(() => { this.randomize(); }, 50);
+  }
 }
