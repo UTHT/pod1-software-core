2b491eafef2c87d5a8f34dd654d9aad12efeeab3
MJavaadAkhtar <javaad.akhtar@mail.utoronto.ca>
2021-02-06 18:07:40 +0000

    Timestamp Added for encapsulation. C++ files modified to match needs for nodejs Websockets

--- software/core/Infrastructure/c++/socket.cpp
+++ software/core/Infrastructure/c++/socket.cpp
@@ -13,40 +13,43 @@ int main()
     client.connect("ws://127.0.0.1:8080/").wait();
     websocket_outgoing_message out_msg;
 
-    out_msg.set_utf8_message("{ 'isNew':1 }");
+    out_msg.set_utf8_message("{ 'eventType':'connection', 'data':'odroid' }");
     client.send(out_msg).wait();
     
     const char *text2 = "\
            {\
-            'serverType':'odroid',\
-              'speed':[\
-                  {\
-                      'name':'speed','value':250\
-                  }\
-              ],\
-              'temperatures':[\
-                            {\
-                              'name': 'motor','value':56 \
-                            },\
-                            {'name': 'battery','value':78} \
-                            ],\
-              'position':[127,46],\
-              'brakes':[\
-                  {\
-                      'name':'brakes_left','status':0,'pressure':257\
-                  },\
-                  {\
-                      'name':'brakes_right','status':0,'pressure':257\
-                  }\
-              ],\
-              'battery':[\
-                  {\
-                      'name':'battery_1','value':67\
-                  },\
-                  {\
-                      'name':'battery_2','value':64\
-                  }\
-              ]\
+           'eventType':'relay',\
+           'data': {\
+              'clientType':'odroid',\
+                'speed':[\
+                    {\
+                        'name':'speed','value':250\
+                    }\
+                ],\
+                'temperatures':[\
+                              {\
+                                'name': 'motor','value':56 \
+                              },\
+                              {'name': 'battery','value':78} \
+                              ],\
+                'position':[127,46],\
+                'brakes':[\
+                    {\
+                        'name':'brakes_left','status':0,'pressure':257\
+                    },\
+                    {\
+                        'name':'brakes_right','status':0,'pressure':257\
+                    }\
+                ],\
+                'battery':[\
+                    {\
+                        'name':'battery_1','value':67\
+                    },\
+                    {\
+                        'name':'battery_2','value':64\
+                    }\
+                ]\
+              }\
           }\
         ";
     
