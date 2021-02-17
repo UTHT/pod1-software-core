b8a107d683174b4de81a57a59923f8947354f83a
ArnavJain23 <arnav.jain@mail.utoronto.ca>
2021-02-17 16:37:33 -0700

    correctly validating temperature, speed, and position data

--- software/core/Infrastructure/backend/validator/validate.js
+++ software/core/Infrastructure/backend/validator/validate.js
@@ -35,129 +35,119 @@ const ValidationError = require('./ValidationErrorClass')
  * @param {[Object object] <JSON>} jsonOdriodData 
  * @returns {... List<Class>} [errors]
  */
-function validate(jsonOdriodData){
+function validate(jsonOdriodData) {
     var errorarray = [];
 
     //structural integrity of data:
-        //sensor type
-        //has name and value
+    //sensor type
+    //has name and value
 
     //call appropriate sensor functions for it respective fields as the sensor type is detected in the for loop
 
     //loop through json data
-        //when sensor type detected call appropriate sensor funtion 
-            //if name detected
-                //if it does not have string throw syntax error and populate error array
-                //else push to data base 
-            //if value detected
-                //if value does not have double/integer throw syntax error and populate error array
-                //else value not detected throw value error and populate error array
-        //if no type detected throw type error and populate error array
-        
-        if ('speed' in jsonOdriodData){
-            errorarray.push(checkSpeedData(jsonOdriodData.speed));
-            // checkSpeedData(jsonOdriodData.speed);
-            // console.log(errorarray);
-        }
-        else{
-            //throw speed type error
-            errorarray.push(new ValidationError('No field: speed').message);
-            // console.log(errorarray);
-        }
+    //when sensor type detected call appropriate sensor funtion 
+    //if name detected
+    //if it does not have string throw syntax error and populate error array
+    //else push to data base 
+    //if value detected
+    //if value does not have double/integer throw syntax error and populate error array
+    //else value not detected throw value error and populate error array
+    //if no type detected throw type error and populate error array
 
-        if ('temperatures' in jsonOdriodData){
-            errorarray.push(checkTempData(jsonOdriodData.temperatures));
-            // checkTempData(jsonOdriodData.temperatures);
-            // console.log(errorarray);
-        }
-        else{
-            //throw temprature type error
-            errorarray.push(new ValidationError('No field: temperatures').message);
-            // console.log(errorarray);
-        }
-
-        if ('position' in jsonOdriodData){
-            errorarray.push(checkPositionData(jsonOdriodData.position));
-            // consol.log(checkPositionData(jsonOdriodData.position));
-            console.log(errorarray);
-        }
-        else{
-            //throw position type error
-            errorarray.push(new ValidationError('No field: position').message);
-        }
-
-        // if ('brakes' in jsonOdriodData){
-        //     errorarray.push(checkBrakeData(jsonOdriodData.brakes));
-
-        // }
-        // else{
-        //     //throw brakes type error
-        // }
-
-        // if ('battery' in jsonOdriodData){
-        //     errorarray.push(checkBatteryData(jsonOdriodData.battery));
-
-        // }
-        // else{
-        //     //throw battery type error
-        // }
-
-    //generate and return error array
-///dont return/send the error data further
-
-
-    //call fuctions to check if less than threshold value 
-    //call respective component's class to check if type is valid
-        //any other fields that are specific to that component
-        //is checked in that class
-
-    
-/* SKIP FOR NOW
-send ALL data to database (regardless of it being correct or incorrect)
-set flags in database that indicate correct and incorrect data
-integrer flag: 0 for bad and 1 for good
-*/
-
-
-}
-
-
-//function: validate speed value
-function checkSpeedData(speedDataArray) {
-    if ('value' in speedDataArray){
-        console.log("speed value exists");
-
-        //check if value is more than 0
-        if (speedDataArray.value > 0){ 
-            console.log("speed value exists");            
-        }
-        else {
-            return new ValidationError('Speed cannot be less than 0').message;
-        }
+    if ('speed' in jsonOdriodData) {
+        errorarray.push(checkSpeedData(jsonOdriodData.speed));
+        // checkSpeedData(jsonOdriodData.speed);
+        // console.log(errorarray);
     }
     else {
-        return new ValidationError('No field: value');
+        //throw speed type error
+        errorarray.push(new ValidationError('No field: speed').message);
+        // console.log(errorarray);
     }
+
+    if ('temperatures' in jsonOdriodData) {
+        errorarray.push(checkTempData(jsonOdriodData.temperatures));
+        // checkTempData(jsonOdriodData.temperatures);
+        // console.log(errorarray);
+    }
+    else {
+        //throw temprature type error
+        errorarray.push(new ValidationError('No field: temperatures').message);
+        // console.log(errorarray);
+    }
+
+    if ('position' in jsonOdriodData) {
+        errorarray.push(checkPositionData(jsonOdriodData.position));
+        // consol.log(checkPositionData(jsonOdriodData.position));
+        // console.log(errorarray);
+    }
+    else {
+        //throw position type error
+        errorarray.push(new ValidationError('No field: position').message);
+    }
+
+    if ('brakes' in jsonOdriodData) {
+        errorarray.push(checkBrakeData(jsonOdriodData.brakes));
+        // console.log(checkBrakeData(jsonOdriodData.brakes));
+        console.log(errorarray);
+    }
+    else {
+        //throw brakes type error
+    }
+
+    // if ('battery' in jsonOdriodData){
+    //     errorarray.push(checkBatteryData(jsonOdriodData.battery));
+
+    // }
+    // else{
+    //     //throw battery type error
+    // }
+
+    //generate and return error array
+    ///dont return/send the error data further
+
 }
 
-//function: validate temp name
-//function: validate temp value
-function checkTempData(temperatureArrayData){
+//IF NO FUNCTIONS RETURN ERROR --> NEED TO FIX UNDEFINED IN ERRORARRAY
+
+//validates speed value
+function checkSpeedData(speedDataArray) {
+    speedDataArray.forEach(elem => {
+        if ('value' in elem) {
+            console.log("speed value exists");
+
+            //check if value is more than 0
+            if (elem.value > 0) {
+                console.log("speed value more than 0: ", elem.value);
+            }
+            else {
+                return new ValidationError('Speed cannot be less than 0').message;
+            }
+        }
+        else {
+            return new ValidationError('No field: value').message;
+        }
+
+    });
+}
+
+//validate temp name and value
+function checkTempData(temperatureArrayData) {
     var temperatureErrors = {}
 
 
     temperatureArrayData.forEach((element, index) => {
-        if ('name' in element){
+        if ('name' in element) {
             // console.log(element.name);
         }
-        else{
+        else {
             temperatureErrors[index] = new ValidationError("No field: name").message;
         }
 
-        if ('value' in element){
+        if ('value' in element) {
             // console.log(element.value);
         }
-        else{
+        else {
             temperatureErrors[index] = new ValidationError("No field: value").message;
         }
     });
@@ -166,19 +156,18 @@ function checkTempData(temperatureArrayData){
 }
 
 
-//function: validate position value
-//function: validate brake name
-function checkPositionData(positionArrayData){
+//validate position value
+function checkPositionData(positionArrayData) {
     var positionErrors = {}
 
     positionArrayData.forEach((elem, index) => {
-        if (elem > 0){
+        if (elem > 0) {
             // console.log(elem);
         }
-        else if (elem < 0){
+        else if (elem < 0) {
             positionErrors[index] = new ValidationError("Position cannot be less than 0").message;
         }
-        else{
+        else {
             positionErrors[index] = new ValidationError("No values provided for position").message;
         }
     });
@@ -188,6 +177,39 @@ function checkPositionData(positionArrayData){
 
 //function: validate brake status
 //function: validate brake value
+function checkBrakeData(brakeArrayData) {
+    var brakeErrors = {}
+
+    brakeArrayData.forEach((element, index) => {
+        for ([key, val] of Object.entries(element)) {
+            if ('name' in element) {
+                // console.log(element.name);
+            }
+            else {
+                brakeErrors["name"] = new ValidationError("No field").message;
+            }
+
+            if ('status' in element) {
+                // console.log(element.status);
+            }
+            else {
+                brakeErrors["status"] = new ValidationError("No field").message;
+            }
+
+            if ('pressure' in element) {
+                // console.log(element.pressure);
+            }
+            else {
+                brakeErrors["pressure"] = new ValidationError("No field").message;
+            }            
+        }
+
+    });
+
+    return brakeErrors;
+
+}
+
 //function: validate battery name
 //function: validate battery value
 //function: validate podstate ???
