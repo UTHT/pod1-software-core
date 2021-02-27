7fe97ee2a5f6721e4cf13ce073293cbd8f8ae70b
BauerJustin <justin.bauer@mail.utoronto.ca>
2021-02-27 14:56:22 -0700

    Loop through several test cases for gauge/graph tests

--- software/core/Infrastructure/AngularFrontend/src/app/components/widgets/graph/graph.component.spec.ts
+++ software/core/Infrastructure/AngularFrontend/src/app/components/widgets/graph/graph.component.spec.ts
@@ -2,57 +2,78 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphComponent } from './graph.component';
 
-describe('GraphComponent', () => {
-  let component: GraphComponent;
-  let fixture: ComponentFixture<GraphComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GraphComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GraphComponent);
-    component = fixture.componentInstance;
-
-    component.settings = {
+const testCases = [
+  {
+    input: [1, 2, 3, 4, 5], settings: {
+      unit: 'm/s',
+      gaugeType: 'arch',
+      gaugeSize: '200',
+      graphWidth: 400,
+      graphHeight: 100,
+      max: 25,
+    }
+  },
+  {
+    input: [0, 2, 4, 6, 8, 10, 12, 14, 1, 6, 18, 20], settings: {
       unit: 'bar',
       gaugeType: 'arch',
       gaugeSize: '125',
       graphWidth: 200,
       graphHeight: 100,
       max: 3,
-    };
-    component.dataList = [1,2,3,4,5];
-
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should be defined', () => {
-    expect(component.dataList).toBeDefined();
-    expect(component.settings).toBeDefined();
-  })
-
-  it('should match input', () => {
-    expect(component.dataList).toEqual([1,2,3,4,5]);
-    expect(component.settings).toEqual({
-      unit: 'bar',
-      gaugeType: 'arch',
-      gaugeSize: '125',
+    }
+  },
+  {
+    input: [], settings: {
+      unit: '°C',
+      gaugeType: 'semi',
+      gaugeSize: '100',
       graphWidth: 200,
       graphHeight: 100,
-      max: 3,
+      max: 100,
+    }
+  }
+]
+
+testCases.forEach(test => {
+  describe('GraphComponent', () => {
+    let component: GraphComponent;
+    let fixture: ComponentFixture<GraphComponent>;
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [GraphComponent]
+      })
+        .compileComponents();
     });
-  })
 
-  it('should match expected output', () => {
-    expect(component.lineChartData[0].data).toEqual([1,2,3,4,5]);
-    expect(component.lineChartOptions.scales.yAxes[0].ticks.max).toEqual(3);
-  })
+    beforeEach(() => {
+      fixture = TestBed.createComponent(GraphComponent);
+      component = fixture.componentInstance;
+
+      component.settings = test.settings;
+      component.dataList = test.input;
+
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should be defined', () => {
+      expect(component.dataList).toBeDefined();
+      expect(component.settings).toBeDefined();
+    })
+
+    it('should match input', () => {
+      expect(component.dataList).toEqual(test.input);
+      expect(component.settings).toEqual(test.settings);
+    })
+
+    it('should match expected output', () => {
+      expect(component.lineChartData[0].data).toEqual(test.input);
+      expect(component.lineChartOptions.scales.yAxes[0].ticks.max).toEqual(test.settings.max);
+    })
+  });
 });
