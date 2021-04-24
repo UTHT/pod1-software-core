ae82478a2578a8981a4c8a44604ce2214973309d
Daman Sharma <damansh@gmail.com>
2021-04-24 01:00:57 -0400

    Arduino is now able to publish to a ZCM channel!

--- software/arduino/sketches/arduino_test/arduino_test.ino
+++ software/arduino/sketches/arduino_test/arduino_test.ino
@@ -4,34 +4,42 @@
 #include <test_msg.h>
 
 zcm_t* zcm_arduino;
+int counter;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);  
-  zcm_arduino = create_zcm(1);
+  zcm_arduino = create_zcm(0);
+  counter = 0;
 }
 
 int publishTestToChannel(zcm_t* zcm, String msg, String channel) {  
   test_msg message;
   message.str = (char *) msg.c_str();
 
-  test_msg_destroy(&message);
-
   return test_msg_publish(zcm, channel.c_str(), &message);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  String message = "This is a test message";
+  String message = "This is a test message: " + String(counter);
 
-  
+  // Might not need to include this. Arduino was able to publish 
+  // with and without this.
+  zcm_handle_nonblock(zcm_arduino);
 
+  // The "send" method in PacketSerialArduino writes an encoded
+  // version of the message to the serial monitor. The Linux side
+  // of ZCM reads this message (aka the Odroid).
   int result = publishTestToChannel(zcm_arduino, message, "TEST");
 
-  if (result == 0) {
-    Serial.println("Successfully published to channel 'TEST'");
-  } else {
-    Serial.println("Error");
-  }
+  // Need to remove this after testing
+  Serial.println();
 
+//  if (result == 0) {
+//    Serial.println("Successfully published to channel 'TEST'");
+//  } else {
+//    Serial.println("Error");
+//  }
+
+  counter++;
+  delay(2000);
 }
