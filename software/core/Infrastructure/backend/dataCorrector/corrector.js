3771642f58594eda1362c547408168991035a934
ArnavJain23 <arnav.jain@mail.utoronto.ca>
2021-05-08 11:33:59 -0600

    Miscellaneous code corrector.js

--- software/core/Infrastructure/backend/dataCorrector/corrector.js
+++ software/core/Infrastructure/backend/dataCorrector/corrector.js
@@ -1,13 +1,37 @@
 const fs = require("fs");
 
+var commonErrors = fs.readFileSync("./commonErrors.json");
+var commonErrorsParsed = JSON.parse(commonErrors);
+var commonErrorkeyArray = Object.keys(commonErrorsParsed);
+
 function corrector(json_odriod_data, error_obj) {
+
 
 }
 
 
+var obj = {
+    "speed": [
+        {
+            "value": -8
+        }
+    ],
+};
+
+var temp_dict = {"name": "speed", "value": 0};
+obj["speed"][0] = temp_dict;
+
+var temp_string = "temp";
+var hello = "hello";
+obj["speed"][0][temp_string] = hello;
+
+console.log(JSON.stringify(obj, null, 4));
+
 var test_data = fs.readFileSync("./test_data.json");
 var test_errors = fs.readFileSync("./test_error.json");
-const correct_object = validate(JSON.parse(test_data), JSON.parse(test_errors));
-console.log(correct_object);
+// var  error_obj = validate(JSON.parse(test_data));
+const correct_object = corrector(JSON.parse(test_data), JSON.parse(test_errors))
+// console.log(correct_object);
 
 module.exports = corrector;
+
