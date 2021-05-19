52020989013c621e573422d1781419de089c78ae
Daman Sharma <damansh@gmail.com>
2021-05-19 18:55:31 -0400

    Added ENUMS to channel class

--- software/communications/channels/channel.cpp
+++ software/communications/channels/channel.cpp
@@ -1,43 +1,44 @@
 #include "channel.hpp"
+#include "enums.hpp"
 //type checking
 #include <typeinfo>
-#include <time.h> 
+#include <ctime> 
 
 
-Channel::Channel(string channel_name, double min_value, double max_value, time_t last_comm_time) {
+Channel::Channel(string channel_name, double min_value, double max_value) {
     this->channel_name = channel_name;
     this->min_value = min_value;
     this->max_value = max_value;
 
-    // TODO: Instead of passing a last_comm_time value to the constructor, can we default this value to NULL or like 0?
-    this->last_comm_time = last_comm_time; 
+    // Defaulted this to the current time
+    time(&this->last_comm_time)
 }
 
 // Should return an ENUM
 int Channel::validateCurrentValue() {
     time_t current_comm_time; 
-    double time_diff; 
 
-    //update the current time 
+    // Update the current time 
     time(&current_comm_time); 
 
-    //get the difference between current time and the last communicated time
-    time_diff = difftime(current_comm_time, this->last_comm_time); 
+    // Get the difference between current time and the last communicated time
+    double time_diff = difftime(current_comm_time, this->last_comm_time); 
 
     // Check time
-    if (time_diff > 60.0) {
-        // return a time ENUM for error checking 
+    if (time_diff >= 60.0) {
+        return ChannelStatus::NO_COMMS;
     }
-
 
     // Check value within range
-    if(this->current_value < this->min_value || this->current_value > this->max_value) {
-        // THIS BAD - do some shit ???? o_O
+    if (this->current_value < this->min_value || this->current_value > this->max_value) {
+        return ChannelStatus::OUT_OF_RANGE;
     }
-    // (Maybe) Check the frequency of data 
 
+    // TODO: (Maybe) Check the frequency of data 
+    // 1) What are the acceptable bounds?
 
-    // DEFAULT: return ALL_GOOD
+    // Default return value
+    return ChannelStatus::FUNCTIONING;
 }
 
 void Channel::callbackHandler(const zcm_recv_buf_t* rbuf, const char* channel, const zcm_msg* msg, void* user) {
@@ -47,11 +48,12 @@ void Channel::callbackHandler(const zcm_recv_buf_t* rbuf, const char* channel, c
 
     // message type checking
     // Need to double check what  typeid.name() returns for double and arrays
-    if (typeid(msg->data_value).name() == "double" || typeid(msg->data_value).name() == "int" ) {
+    if (typeid(msg->data_value).name() == "double" || typeid(msg->data_value).name() == "int") {
         this->current_value = msg->data_value;
+        time(&this->last_comm_time);
     } else {
         //returning some string or gibberish
-         this->current_value = -1;
+        this->current_value = -1;
     }
 }
 
