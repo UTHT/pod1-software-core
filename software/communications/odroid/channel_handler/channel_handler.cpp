fcf53fe9ad5de2b55d0d5bc16c90d194ad89759e
Devendra Nanga <devnanga@gmail.com>
2021-06-12 00:59:27 -0400

    Got json files working

--- software/communications/odroid/channel_handler/channel_handler.cpp
+++ software/communications/odroid/channel_handler/channel_handler.cpp
@@ -1,5 +1,6 @@
 // Implementation of channel handler
 #include "channel_handler.hpp"
+#include "nlohmann/json.hpp"
 
 unordered_map<string, Channel*> channel_map;
 
@@ -19,13 +20,33 @@ void add_to_channel(Channel* temp) {
 }
 
 void populate_channel_map() {
-    Channel *temp = new Channel("temperature_1", "/dev/cu.usbmodem14501", 10, 30);
-    Channel *temp1 = new Channel("voltage_1", "/dev/cu.usbmodem14501", 0, 5);
-    Channel *temp2 = new Channel("resistance_1", "/dev/cu.usbmodem14501", 0, 10);
 
-    add_to_channel(temp); 
-    add_to_channel(temp1); 
-    add_to_channel(temp2); 
+    //Code to open and read from json file channels.json
+    std::ifstream i ("channels.json");
+    nlohmann::json j;
+    i>>j;        
+    int x; 
+
+    //Iterate through json object and assign values accessed through json type
+    for( x = 0; x < j["channels"].size(); x++){       
+
+        std::string name = j["channels"][x]["sensor"];
+        std::string port = j["channels"][x]["serial_port"];
+        double min_value = j["channels"][x]["min_value"];
+        double max_value = j["channels"][x]["max_value"];
+
+        Channel *temp = new Channel(name ,port,min_value ,max_value);
+        add_to_channel(temp);
+
+    }    
+
+    // Channel *temp = new Channel("temperature_1", "/dev/cu.usbmodem14501", 10, 30);
+    // Channel *temp1 = new Channel("voltage_1", "/dev/cu.usbmodem14501", 0, 5);
+    // Channel *temp2 = new Channel("resistance_1", "/dev/cu.usbmodem14501", 0, 10);
+
+    // add_to_channel(temp); 
+    // add_to_channel(temp1); 
+    // add_to_channel(temp2); 
 }
 
 int main() {
