bab32f711f28e6d16368a3fe4501513c2e9abac7
Devendra Nanga <devna@MacBook-Air.local>
2021-08-21 14:57:15 -0400

    Changed files to add zcm to vfs

--- software/communications/arduino/sketches/VFS500/VFS500.ino
+++ software/communications/arduino/sketches/VFS500/VFS500.ino
@@ -1,6 +1,10 @@
 // Headers for each sensor type
 #include "src/VFS500.h" 
 //...
+#include <zcm_utht.h>
+#include <transport.h>
+#include <arduino_cobs_serial_transport.hpp>
+#include <channel_array_msg.h>
 
 #include "src/Sensor.h"
 #define NUMSENSORS 1 //Or however many
@@ -28,13 +32,13 @@ void setup(){
         // Print/send sensor post-setup state data here. For example:
         bool _success = (state->error == ERR_NONE);
         if(_success){
-            Serial.print("Sensor ");
-            Serial.print(sensors[i]->sensor);
-            Serial.println(" initialized.");
+            // Serial.print("Sensor ");
+            // Serial.print(sensors[i]->sensor);
+            // Serial.println(" initialized.");
         } else {
-            Serial.print("Sensor ");
-            Serial.print(sensors[i]->sensor);
-            Serial.println(" failed to initialize!");
+            // Serial.print("Sensor ");
+            // Serial.print(sensors[i]->sensor);
+            // Serial.println(" failed to initialize!");
         }
         success &= _success;
     }
@@ -44,22 +48,40 @@ void setup(){
     }
 }
 
+int publishMessageToChannel(zcm_t* zcm, double values[], String channel, String units, int numData) {  
+  channel_array message;
+  message.arduino_id = THISARDUINO;
+  message.sensor_id = A0;
+  message.data = values;
+  message.units = (char *) units.c_str();
+  message.sz = numData;
+
+  return channel_array_publish(zcm, channel.c_str(), &message);
+}
+
 void loop(){
     for(int i = 0; i < NUMSENSORS; i++){
         SensorState* state = sensors[i]->update();
         // Print/send sensor post-setup state data here. For example:
         bool _success = (state->error == ERR_NONE);
         bool _new = (state->debug == DS_SUCCESS);
-        if(_success && _new) {
-            Serial.print("Sensor ");
-            Serial.print(sensors[i]->sensor);
-            Serial.print(" read success: ");
-            for(int x = 0; x < state->numdata; x++){
-                Serial.print(state->data[x].data);
-                Serial.print(' ');
-                Serial.print(state->data[x].units);
-                if(x < state->numdata-1){Serial.print(", ");}else{Serial.println();}
+        if(_success) {
+
+            if(_new){
+                double values[state->numdata];
+                String units = "";
+
+                for(int x = 0; x < state->numdata; x++){
+                    values[x] = state->data[x].data; 
+
+                    units += state->data[x].units;
+                    units += ", ";
+                }
+
+                publishMessageToChannel(zcm_arduino, values, "test_sensor", units, state->numdata);
             }
+            
+            
         } else if (!_success) {
             Serial.print("Sensor ");
             Serial.print(sensors[i]->sensor);
