7251a646c014a6f52dc5a1403a3501651b601da1
Work <work@Mohammads-MacBook-Pro.local>
2022-04-13 14:50:28 -0400

    components are fixed after optimization

--- software/core/Infrastructure/UTHT/src/app/components/dashboard/brake/brake-graph/brake-graph.component.ts
+++ software/core/Infrastructure/UTHT/src/app/components/dashboard/brake/brake-graph/brake-graph.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
-
+import * as pluginAnnotations from 'chartjs-plugin-annotation';
 @Component({
   selector: 'brake-graph',
   templateUrl: './brake-graph.component.html',
@@ -12,53 +12,36 @@ export class BrakeGraphComponent implements OnInit {
   @Input()
   dataList: any[];
 
-  public lineChartData: ChartDataSets[] = [{ data: [], fill:false, pointStyle:'cross'}];
+  public lineChartData: ChartDataSets[] = [{ data: [] }];
   public lineChartLabels: Label[] = [];
-  public lineChartOptions: (ChartOptions) = {
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
     scales: {
-      xAxes: [
-        {
-          type: 'time',
-          distribution: 'linear',
-          time: {
-            unit: "second",
-            displayFormats: {
-              second: 'h:mm:ss'
-          }
-          },
-          ticks:{
-            autoSkipPadding: 20,
-          }
-        }
-      ],
+      xAxes: [{}],
       yAxes: [
         {
+          id: 'y-axis-0',
           position: 'left',
-          ticks:{
-            autoSkipPadding: 20,
-            min: 0,
-            max: 3,
-          }
         }
       ]
     },
-    elements:{
-      line:{
-        tension:0
-      }
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno'
+          }
+        },
+      ],
     },
-    legend: {
-      display: false
-    },
-    animation:{
-      duration: 0
-    },
-    plugins: {
-      datalabels: {
-        display: false,
-      }
-    }
   };
   public lineChartColors: Color[] = [
     {
@@ -72,14 +55,78 @@ export class BrakeGraphComponent implements OnInit {
   ];
   public lineChartLegend = false;
   public lineChartType: ChartType = 'line';
-  public lineChartPlugins = [pluginDataLabels];
+  public lineChartPlugins = [pluginAnnotations];
+
+
+  // public lineChartData: ChartDataSets[] = [{ data: [], fill:false, pointStyle:'cross'}];
+  // public lineChartLabels: Label[] = [];
+  // public lineChartOptions: (ChartOptions) = {
+  //   responsive: true,
+  //   scales: {
+  //     xAxes: [
+  //       {
+  //         type: 'time',
+  //         distribution: 'linear',
+  //         time: {
+  //           unit: "second",
+  //           displayFormats: {
+  //             second: 'h:mm:ss'
+  //         }
+  //         },
+  //         ticks:{
+  //           autoSkipPadding: 20,
+  //         }
+  //       }
+  //     ],
+  //     yAxes: [
+  //       {
+  //         position: 'left',
+  //         ticks:{
+  //           autoSkipPadding: 20,
+  //           min: 0,
+  //           max: 3,
+  //         }
+  //       }
+  //     ]
+  //   },
+  //   elements:{
+  //     line:{
+  //       tension:0
+  //     }
+  //   },
+  //   legend: {
+  //     display: false
+  //   },
+  //   animation:{
+  //     duration: 0
+  //   },
+  //   plugins: {
+  //     datalabels: {
+  //       display: false,
+  //     }
+  //   }
+  // };
+  // public lineChartColors: Color[] = [
+  //   {
+  //     backgroundColor: 'rgba(0, 255, 0, 0.3)',
+  //     borderColor: 'green',
+  //     pointBackgroundColor: 'rgba(148,159,177,1)',
+  //     pointBorderColor: '#fff',
+  //     pointHoverBackgroundColor: '#fff',
+  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+  //   }
+  // ];
+  // public lineChartLegend = false;
+  // public lineChartType: ChartType = 'line';
+  // public lineChartPlugins = [pluginDataLabels];
 
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.lineChartData[0].data = this.dataList
+    // this.lineChartData[0].data = this.dataList
+    this.getBrakePressureArray();
   }
 
   processLineChartData(dataList) {
