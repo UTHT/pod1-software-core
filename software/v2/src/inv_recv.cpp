d3fec4a5e4e7684725850508dc5c5b985097658a
sabitatasnim <sabita.tasnim@gmail.com>
2022-06-07 21:00:22 -0400

    removed src/inv_recv.cpp

--- software/v2/src/inv_recv.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "inv_data.hpp"
-
-
-int update_broadcast_data(unsigned long id, unsigned char dta[8]){
-    if (id == 0x0A0){
-        inv_data.temp_a = (dta[0] + dta[1] << 8) / 10;
-    }
-}
